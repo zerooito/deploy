44561f567e3d4959edfa6ab041956efbb69cf3a0
Reginaldo Junior <76regi@gmail.com>
2017-09-23 21:28:37 -0300

    add file to control deploys

--- scrutinizer.php
+++ scrutinizer.php
@@ -17,15 +17,41 @@ class Deploy
 
         $output = json_decode(curl_exec($curl)); 
 
-        curl_close($curl);      
+        curl_close($curl);
+
+        $lastCommit = $this->getLastCommit();
+        
+        $currentCommit = $this->saveCurrentCommit($output->_embedded->inspections[0]->metadata->source_reference);
+
+        if ($lastCommit == $currentCommit) {
+        	print "Already updated" . "\n";
+        	exit;
+        }
 
         return $output->_embedded->inspections[0]->build->status;
 	}
 
+	public function getLastCommit()
+	{
+		return fgets(fopen('commit.txt', 'r'));
+	}
+
+	public function saveCurrentCommit($commit)
+	{
+		$file = fopen("commit.txt", "w") or die("Unable to open file!");
+				
+		fwrite($file, $commit);
+
+		fclose($file);
+
+		return $commit;
+	}
+
 	public function run()
 	{
-		if ($this->getLastBuild() != "passed")
+		if ($this->getLastBuild() != "passed") {
 			throw new Exception("Last Build Failed");			
+		}
 		
 		shell_exec('cd ' . PATH_DEPLOY_SOURCE . ' && dep deploy:unlock -vvv');
 		shell_exec('cd ' . PATH_DEPLOY_SOURCE . ' && dep deploy -vvv');
